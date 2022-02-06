5f79a2cfab9af8734b6bc0ebb87ea3ff8c6b0aa6
khaoula <khaoulajabour4@gmail.com>
2022-02-06 02:55:42 -0500

    create login/signup template

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const path = require('path');
+const exphbs= require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 
 
 
@@ -28,6 +30,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 //turn on routes
 app.use((routes));
 
