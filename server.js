18281beb257746adf89e290232fbad4e764a3e80
khaoula <khaoulajabour4@gmail.com>
2022-02-10 02:11:12 -0500

    update server 2

--- server.js
+++ server.js
@@ -40,6 +40,4 @@ app.use((routes));
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-  }).catch(err=>{
-    console.log(err);
   });
